7785890a432c434eaabf7e4c9f2e9340bf533c8a
OKEDIYA MESHACH <95497067+olubiyi09@users.noreply.github.com>
2023-10-19 03:57:59 +0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["http://localhost:3000", "https://my-pinvent-app.onrender.com"],
+    origin: ["http://localhost:3000", "https://pinvent-app.netlify.app"],
     credentials: true
 }))
 
